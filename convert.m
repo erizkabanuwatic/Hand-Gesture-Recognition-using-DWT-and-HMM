c93e73f19aa890101f9f380d8b42c49f2843f017
Erizka Banuwati Candrasari <44527320+erizkabanuwatic@users.noreply.github.com>
2019-11-14 16:02:05 +0700

    Delete convert.m

--- convert.m
+++
@@ -1,4 +0,0 @@
-
-    filename = strcat('C:\Users\Erizka Banuwati C\Desktop\referensi TA\Referensi TA terbaru\Kodingan\SOURCE CODE TA\Dataset-Erizka\A-Erizka\A-erizka',num2str(i),'.jpg');
-    im  = imread(filename);
-    im2 = imwrite(im,'A-erizka1.bmp');
